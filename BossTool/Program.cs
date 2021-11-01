7b44b4d00667d28403705d80c936ffeca1f750e0
OatmealDome <julian@oatmealdome.me>
2021-10-31 21:05:42 -0400

    Program: Add root command with options

--- BossTool/Program.cs
+++ BossTool/Program.cs
@@ -1,12 +1,34 @@
 ﻿using System;
+using System.CommandLine;
+using System.CommandLine.Invocation;
+using System.IO;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Threading.Tasks;
 
 namespace BossTool
 {
     class Program
     {
-        static void Main(string[] args)
+        static async Task Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Command command = new RootCommand()
+            {
+                new Option<string>("--boss-key", () => null, "The BOSS encryption key."),
+                new Option<string>("--boss-hmac-key", () => null, "The BOSS HMAC key."),
+                new Argument<FileInfo>("file", "The file to decrypt.")
+            };
+
+            command.Handler = CommandHandler.Create((string bossKey, string bossHmacKey, FileInfo file) =>
+            {
+                Run(bossKey, bossHmacKey, file);
+            });
+
+            await command.InvokeAsync(args);
+        }
+
+        private static void Run(string bossKeyString, string hmacKeyString, FileInfo info)
+        {
         }
     }
 }
